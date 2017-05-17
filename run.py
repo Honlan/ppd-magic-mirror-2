37393d09e3ebc124373127c1373472069caa727f
Honlan <honlan@HonlandeMacBook-Air.local>
2017-05-17 11:10:49 +0800

    api test

--- run.py
+++ run.py
@@ -88,8 +88,7 @@ def auth():
 	sort_data = rsa.sort(data)
 	sign = rsa.sign(sort_data)
 	list_result = client.send(access_url, json.dumps(data), APPID, sign)
-	Username = list_result
-	# UserName = rsa.decrypt(list_result[list_result.find('<UserName>') + len('<UserName>'):list_result.find('</UserName>')])
+	Username = rsa.decrypt(list_result[list_result.find('<UserName>') + len('<UserName>'):list_result.find('</UserName>')])
 
 	session['Username'] = Username
 
